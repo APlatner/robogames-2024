40c109878c386aa67d706e96eabd079082d7d2d7
APlatner <trinitrokid@gmail.com>
2023-12-27 17:58:44 -0700

    added engine sounds, and made them play based on the whether the tank moves

--- tank/tank.gd
+++ tank/tank.gd
@@ -5,6 +5,11 @@ extends CharacterBody3D
 
 @export var _suspension_links: Array[SuspensionArm]
 
+@export var _engine_idle: AudioStream
+@export var _engine_full_speed: AudioStream
+@export var _engine_rev_up: AudioStream
+@export var _engine_rev_down: AudioStream
+
 const MAX_LINEAR_SPEED: float = 5
 const MAX_ANGULAR_SPEED: float = 5
 
@@ -26,9 +31,95 @@ var _roll_accel: float
 var _roll_stiffness: float = 3
 var _roll_damping: float = 6
 
+enum EngineState {
+	IDLE,
+	REV_UP,
+	REV_DOWN,
+	FULL_SPEED,
+}
+
+var _left_engine_state = IDLE
+var _left_prev_engine_state = IDLE
+var _right_engine_state = IDLE
+var _right_prev_engine_state = IDLE
+
+func _ready() -> void:
+	$LeftAudioStreamPlayer3D.stream = _engine_idle
+	$LeftRevAudioStreamPlayer3D.stream = _engine_rev_up
+	$RightAudioStreamPlayer3D.stream = _engine_idle
+	$RightRevAudioStreamPlayer3D.stream = _engine_rev_up
+
+func _process(_delta: float) -> void:
+	var left_audio = $LeftAudioStreamPlayer3D
+	var left_rev_audio = $LeftRevAudioStreamPlayer3D
+	var right_audio = $RightAudioStreamPlayer3D
+	var right_rev_audio = $RightRevAudioStreamPlayer3D
+	var forward_input = Input.get_axis("backward", "forward")
+	var horizontal_input = Input.get_axis("left", "right")
+
+	if forward_input == 0.0 and horizontal_input == 0.0:
+		_left_engine_state = IDLE
+		if left_audio.stream == _engine_full_speed and left_audio.playing:
+			left_rev_audio.play()
+			left_audio.stop()
+			left_audio.stream = _engine_idle
+		elif left_rev_audio.stream == _engine_rev_down and not left_rev_audio.playing:
+			left_audio.play()
+			left_rev_audio.stream = _engine_rev_up
+		elif left_audio.stream == _engine_idle and not left_audio.playing:
+			left_audio.play()
+		if right_audio.stream == _engine_full_speed and right_audio.playing:
+			right_rev_audio.play()
+			right_audio.stop()
+			right_audio.stream = _engine_idle
+		elif right_rev_audio.stream == _engine_rev_down and not right_rev_audio.playing:
+			right_audio.play()
+			right_rev_audio.stream = _engine_rev_up
+		elif right_audio.stream == _engine_idle and not right_audio.playing:
+			right_audio.play()
+	elif (forward_input != 0.0 and horizontal_input == 0.0) or (forward_input == 0.0 and horizontal_input != 0.0):
+		if left_audio.stream == _engine_idle and left_audio.playing:
+			left_rev_audio.play()
+			left_audio.stop()
+			left_audio.stream = _engine_full_speed
+		elif left_rev_audio.stream == _engine_rev_up and not left_rev_audio.playing:
+			left_audio.play()
+			left_rev_audio.stream = _engine_rev_down
+		elif left_audio.stream == _engine_full_speed and not left_audio.playing:
+			left_audio.play()
+		if right_audio.stream == _engine_idle and right_audio.playing:
+			right_rev_audio.play()
+			right_audio.stop()
+			right_audio.stream = _engine_full_speed
+		elif right_rev_audio.stream == _engine_rev_up and not right_rev_audio.playing:
+			right_audio.play()
+			right_rev_audio.stream = _engine_rev_down
+		elif right_audio.stream == _engine_full_speed and not right_audio.playing:
+			right_audio.play()
+	elif forward_input != 0.0 and horizontal_input != 0.0:
+		if left_audio.stream == _engine_full_speed and left_audio.playing:
+			left_rev_audio.play()
+			left_audio.stop()
+			left_audio.stream = _engine_idle
+		elif left_rev_audio.stream == _engine_rev_down and not left_rev_audio.playing:
+			left_audio.play()
+			left_rev_audio.stream = _engine_rev_up
+		elif left_audio.stream == _engine_idle and not left_audio.playing:
+			left_audio.play()
+		if right_audio.stream == _engine_idle and right_audio.playing:
+			right_rev_audio.play()
+			right_audio.stop()
+			right_audio.stream = _engine_full_speed
+		elif right_rev_audio.stream == _engine_rev_up and not right_rev_audio.playing:
+			right_audio.play()
+			right_rev_audio.stream = _engine_rev_down
+		elif right_audio.stream == _engine_full_speed and not right_audio.playing:
+			right_audio.play()
+
 func _physics_process(delta: float) -> void:
 	var target_linear_speed = Input.get_axis('backward', 'forward') * MAX_LINEAR_SPEED
 	var target_angular_speed = -Input.get_axis('left', 'right') * MAX_ANGULAR_SPEED
+
 	_linear_speed = _handle_any_accel(_linear_speed, target_linear_speed, _linear_accel * delta, MAX_LINEAR_SPEED)
 	_angular_speed = _handle_any_accel(_angular_speed, target_angular_speed, _angular_accel * delta, MAX_ANGULAR_SPEED)
 	velocity = _linear_speed * global_basis.z
