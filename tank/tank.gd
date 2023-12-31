6aab3e0c52e633ea5ba9e8c396848555daafbb7f
APlatner <trinitrokid@gmail.com>
2023-12-30 17:50:15 -0700

    reorganized the AudioStreamPlayer nodes to sit under a Node3D for better organization. Made the engine rev up sound effect only play when the tank is actually going in the direction of the input. Removed some of the elif statements in the _process function. Preloaded the audio streams to improve readability, and got the length of the rev up/down sound effects using onready.

--- tank/tank.gd
+++ tank/tank.gd
@@ -29,36 +29,41 @@ var _roll_damping: float = 6
 var _rev_up_anim: float = 0.0
 var _rev_down_anim: float = 1.0
 
+@onready var _engine_idle: AudioStreamPlayer3D = $EngineAudio/Idle
+@onready var _engine_full_speed: AudioStreamPlayer3D = $EngineAudio/FullSpeed
+@onready var _engine_rev_up: AudioStreamPlayer3D = $EngineAudio/RevUp
+@onready var _engine_rev_down: AudioStreamPlayer3D = $EngineAudio/RevDown
+@onready var rev_up_length := _engine_rev_up.stream.get_length()
+@onready var rev_down_length := _engine_rev_down.stream.get_length()
+
 func _ready() -> void:
-	$EngineIdleAudio.play()
-	$EngineFullSpeedAudio.play()
-	$EngineFullSpeedAudio.volume_db = -80
+	_engine_idle.play()
+	_engine_full_speed.play()
+	_engine_full_speed.volume_db = linear_to_db(0.0)
 
 func _process(delta: float) -> void:
-	var linear_input = Input.get_axis('backward', 'forward')
-	var angular_input = -Input.get_axis('left', 'right')
-	var rev_up_length = $EngineRevUpAudio.stream.get_length()
-	var rev_down_length = $EngineRevDownAudio.stream.get_length()
+	var linear_input = Input.get_axis("backward", "forward")
+	var angular_input = -Input.get_axis("left", "right")
 	if linear_input == 0.0 and angular_input == 0.0 and _rev_down_anim == 0.0:
 		_rev_up_anim = 0.0
-		$EngineRevDownAudio.play()
-		$EngineRevUpAudio.stop()
-		$EngineFullSpeedAudio.volume_db = -80
+		_engine_rev_down.play()
+		_engine_rev_up.stop()
 		_rev_down_anim += delta / rev_down_length
-	elif linear_input == 0.0 and angular_input == 0.0 and _rev_down_anim != 0.0:
-		_rev_down_anim = clampf(_rev_down_anim, 0.0, 1.0)
-		$EngineIdleAudio.volume_db = -80 + 80 * _rev_down_anim
-		_rev_down_anim += delta / rev_down_length
-	elif (linear_input != 0.0 or angular_input != 0.0) and _rev_up_anim == 0.0:
+	elif (linear_input / _linear_speed > 0 or angular_input / _angular_speed > 0) and _rev_up_anim == 0.0:
 		_rev_down_anim = 0.0
-		$EngineRevUpAudio.play()
-		$EngineRevDownAudio.stop()
-		$EngineIdleAudio.volume_db = -80
+		_engine_rev_up.play()
+		_engine_rev_down.stop()
 		_rev_up_anim += delta / rev_up_length
-	elif (linear_input != 0.0 or angular_input != 0.0) and _rev_up_anim != 0.0:
-		_rev_up_anim = clampf(_rev_up_anim, 0.0, 1.0)
-		$EngineFullSpeedAudio.volume_db = -80 + 80 * _rev_up_anim
-		_rev_up_anim += delta / rev_up_length
+
+	_engine_idle.volume_db = linear_to_db(_rev_down_anim)
+	_rev_down_anim += delta / rev_down_length * ceil(_rev_down_anim)
+	_rev_down_anim = clampf(_rev_down_anim, 0.0, 1.0)
+
+	_engine_full_speed.volume_db = linear_to_db(_rev_up_anim)
+	_rev_up_anim += delta / rev_up_length * ceil(_rev_up_anim)
+	_rev_up_anim = clampf(_rev_up_anim, 0.0, 1.0)
+	_rev_up_anim = _rev_up_anim * float(!(linear_input / _linear_speed < 0 and angular_input / _angular_speed < 0))
+
 
 func _physics_process(delta: float) -> void:
 	var target_linear_speed = Input.get_axis('backward', 'forward') * MAX_LINEAR_SPEED
