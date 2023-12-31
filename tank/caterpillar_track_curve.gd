be6ab99d113dcbcfeddcb7798723b7f64d7047b3
Jonathan Platner <jonathan.platner@outlook.com>
2023-12-30 19:00:17 -0800

    update caterpillar_track_curve.gd

--- tank/caterpillar_track_curve.gd
+++ tank/caterpillar_track_curve.gd
@@ -1,11 +1,10 @@
+class_name CaterpillarTrackCurve
 extends Path3D
 
 @export_enum("Left", "Right") var _side: String = "Left"
+
 var _rollers: Array[Wheel]
-
-var a: float = 0
 var q: float = 0.5 # Droop amount across upper rollers
-
 var _radii: Array[float] # Array of wheel radii
 
 func _ready() -> void:
@@ -16,25 +15,28 @@ func _ready() -> void:
 	_generate_curve_points()
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	_generate_curve_points()
 
 
 func _generate_curve_points():
 	curve.clear_points()
-	var p: Array[Vector3]
-	var l: Array[Vector3] # Array of roller offset vectors
-	var alpha: Array[float]
-	var beta: Array[float]
-	var u: Array[Vector3] # First point on roller
-	var v: Array[Vector3] # Second point on roller
-	var n: Array[float]
-	var k: Array[float] # Scalar length of tangent handles on circular bezier segments
-	var f: Array[Vector3] # Outgoing tangent handles
-	var g: Array[Vector3] # Incoming tangent handles
+	var p: Array[Vector3] = []
+	var l: Array[Vector3] = [] # Array of roller offset vectors
+	var alpha: Array[float] = []
+	var beta: Array[float] = []
+	var u: Array[Vector3] = [] # First point on roller
+	var v: Array[Vector3] = [] # Second point on roller
+	var n: Array[float] = []
+	var k: Array[float] = [] # Scalar length of tangent handles on circular bezier segments
+	var f: Array[Vector3] = []# Outgoing tangent handles
+	var g: Array[Vector3] = []# Incoming tangent handles
+	var i_next: int
+	var i_prev: int
+
 	for i in len(_rollers):
-		var i_next := wrapi(i + 1, 0, 4) # i + 1 wrapped around
-		var i_prev := wrapi(i - 1, 0, 4) # i - 1 wrapped around
+		i_next = wrapi(i + 1, 0, 4)
+		i_prev = wrapi(i - 1, 0, 4)
 		p.append(to_local(_rollers[i].global_position))
 		l.append(to_local(_rollers[i].global_position) - to_local(_rollers[i_next].global_position))
 		l[i].x = 0
@@ -47,8 +49,8 @@ func _generate_curve_points():
 		beta.append(atan2(l[i].y, l[i].z))
 
 	for i in 4:
-		var i_next := wrapi(i + 1, 0, 4) # i + 1 wrapped around
-		var i_prev := wrapi(i - 1, 0, 4) # i - 1 wrapped around
+		i_next = wrapi(i + 1, 0, 4) # i + 1 wrapped around
+		i_prev = wrapi(i - 1, 0, 4) # i - 1 wrapped around
 		u.append(
 			_radii[i] * Vector3(
 				0,
@@ -63,7 +65,6 @@ func _generate_curve_points():
 				cos(beta[i] + alpha[i] + (q if i == 3 else 0.0))
 			)
 		)
-
 		n.append(
 			2 * PI / (
 				fmod(beta[i_prev] + alpha[i_prev], 2 * PI)
@@ -73,7 +74,6 @@ func _generate_curve_points():
 				)
 		)
 		k.append(4.0 / 3.0 * tan(PI / 2 / n[i]))
-
 		f.append(k[i] * Vector3(0, -u[i].z, u[i].y))
 		g.append(k[i] * Vector3(0, v[i].z, -v[i].y))
 
