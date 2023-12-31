e9a1fbc33e993917875323a9b58c13d3557fdffc
Jonathan Platner <jonathan.platner@outlook.com>
2023-12-31 10:54:01 -0800

    move bouncing out of main script

--- tank/physics_response.gd
+++ tank/physics_response.gd
@@ -1,5 +1,72 @@
 extends Node3D
 
-@export var pitch_damped_spring: DampedSpringParameters
-@export var roll_damped_spring: DampedSpringParameters
-@export var z_damped_spring: DampedSpringParameters
+@export var _pitch_damped_spring: DampedSpringParameters
+@export var _roll_damped_spring: DampedSpringParameters
+@export var _z_damped_spring: DampedSpringParameters
+
+var _linear_speed: float
+var _previous_linear_speed: float
+var _linear_accel: Vector2
+
+@onready var left_rear_suspension_link := get_node("Pitch/Mesh/Chassis/LeftRearSuspensionArm") as SuspensionArm
+@onready var left_front_suspension_link := get_node("Pitch/Mesh/Chassis/LeftFrontSuspensionArm") as SuspensionArm
+@onready var right_rear_suspension_link := get_node("Pitch/Mesh/Chassis/RightRearSuspensionArm") as SuspensionArm
+@onready var right_front_suspension_link := get_node("Pitch/Mesh/Chassis/RightFrontSuspensionArm") as SuspensionArm
+@onready var pitch_node := get_node("Pitch") as Node3D
+
+## TODO: check if pitch/z/roll have changed enough - if they have, update the treads
+## TODO: store a reference to parent instead of grabbing it each time
+## TODO: make suspension links an array
+## TODO: move all caterpillar track and shock motion here if needed
+## TODO: z-height
+## TODO: main signals
+func _physics_process(delta: float) -> void:
+	_calculate_spring_position(
+			delta,
+			_pitch_damped_spring,
+			_linear_accel.y * 2)
+	_calculate_spring_position(
+			delta,
+			_roll_damped_spring,
+			_linear_accel.x / 55)
+	pitch_node.rotate_x(deg_to_rad(_pitch_damped_spring.velocity))
+	rotate_z(deg_to_rad(_roll_damped_spring.velocity))
+	left_rear_suspension_link.handle_link_rotation(
+			get_parent_node_3d(),
+			_pitch_damped_spring.position
+		)
+	right_rear_suspension_link.handle_link_rotation(
+			get_parent_node_3d(),
+			_pitch_damped_spring.position
+		)
+	left_front_suspension_link.handle_link_rotation(
+			get_parent_node_3d(),
+			_pitch_damped_spring.position
+		)
+	right_front_suspension_link.handle_link_rotation(
+			get_parent_node_3d(),
+			_pitch_damped_spring.position
+		)
+	_previous_linear_speed = _linear_speed
+
+
+func _calculate_spring_position(
+		delta: float,
+		damped_spring: DampedSpringParameters,
+		external_accel: float = 0,
+		):
+	damped_spring.acceleration = (
+		- damped_spring.position * damped_spring.stiffness * delta
+		- damped_spring.velocity * damped_spring.damping * delta
+		+ external_accel
+		)
+	damped_spring.velocity += damped_spring.acceleration
+	damped_spring.position += damped_spring.velocity
+
+
+func _on_root_linear_speed_changed(speed: float) -> void:
+	_linear_speed = speed
+
+
+func _on_root_linear_accel_changed(accel: Vector2) -> void:
+	_linear_accel = accel
--- tank/tank.gd
+++ tank/tank.gd
@@ -1,49 +1,56 @@
 extends CharacterBody3D
 
+signal linear_speed_changed(speed: float)
+signal linear_accel_changed(accel: Vector2)
+signal angular_speed_changed(speed: float)
+
 @export var _linear_accel: float = 5
 @export var _angular_accel: float = 5
 
-var _suspension_links: Array[SuspensionArm]
+#var _suspension_links: Array[SuspensionArm]
 
 const MAX_LINEAR_SPEED: float = 5
 const MAX_ANGULAR_SPEED: float = 5
 
-var _linear_speed: float
+var _linear_speed: float:
+	set(value):
+		_linear_speed = value
+		linear_speed_changed.emit(_linear_speed)
 var _previous_linear_speed: float
 var _angular_speed: float
 
-var _current_accel: Vector2
+var _current_accel: Vector2:
+	set(value):
+		_current_accel = value
+		linear_accel_changed.emit(_current_accel)
 
 # TODO: make this a resource container
-var _pitch_angle: float
-var _pitch_speed: float
-var _pitch_accel: float
-var _pitch_stiffness: float = 3
-var _pitch_damping: float = 9
-
-var _roll_angle: float
-var _roll_speed: float
-var _roll_accel: float
-var _roll_stiffness: float = 3
-var _roll_damping: float = 6
-
-var _bounce_height: float
-var _bounce_speed: float
-var _bounce_accel: float
-var _bounce_stiffness: float = 6
-var _bounce_damping: float = 12
-
-
-func _enter_tree() -> void:
-	#set_scripts_on_mesh_children()
-	pass
+#@export var _pitch_damped_spring: DampedSpringParameters
+#var _pitch_angle: float
+#var _pitch_speed: float
+#var _pitch_accel: float
+##var _pitch_stiffness: float = 3
+##var _pitch_damping: float = 9
+#
+#var _roll_angle: float
+#var _roll_speed: float
+#var _roll_accel: float
+#var _roll_stiffness: float = 3
+#var _roll_damping: float = 6
+#
+#var _bounce_height: float
+#var _bounce_speed: float
+#var _bounce_accel: float
+#var _bounce_stiffness: float = 6
+#var _bounce_damping: float = 12
 
 
-func _ready() -> void:
-	_suspension_links.append($Roll/Pitch/Mesh/Chassis/LeftFrontSuspensionArm)
-	_suspension_links.append($Roll/Pitch/Mesh/Chassis/RightFrontSuspensionArm)
-	_suspension_links.append($Roll/Pitch/Mesh/Chassis/LeftRearSuspensionArm)
-	_suspension_links.append($Roll/Pitch/Mesh/Chassis/RightRearSuspensionArm)
+
+#func _ready() -> void:
+	#_suspension_links.append($Roll/Pitch/Mesh/Chassis/LeftFrontSuspensionArm)
+	#_suspension_links.append($Roll/Pitch/Mesh/Chassis/RightFrontSuspensionArm)
+	#_suspension_links.append($Roll/Pitch/Mesh/Chassis/LeftRearSuspensionArm)
+	#_suspension_links.append($Roll/Pitch/Mesh/Chassis/RightRearSuspensionArm)
 
 
 func _physics_process(delta: float) -> void:
@@ -53,59 +60,65 @@ func _physics_process(delta: float) -> void:
 	_angular_speed = _handle_any_accel(_angular_speed, target_angular_speed, _angular_accel * delta, MAX_ANGULAR_SPEED)
 	velocity = _linear_speed * global_basis.z
 	rotate(transform.basis.y, _angular_speed * delta)
-	position.y = _bounce_height
+	#position.y = _bounce_height
 	_calc_accel()
-	_handle_pitch(delta)
-	_tilt()
+
+	#_handle_custom_physics(delta)
+	#_tilt()
 	_previous_linear_speed = _linear_speed
 	_wheels_up()
 	move_and_slide()
-	if Input.is_action_just_pressed('shoot'):
-		_bounce_speed = -0.05
+	#if Input.is_action_just_pressed('shoot'):
+		#_bounce_speed = -0.05
 
 
 func _wheels_up():
-	for link in _suspension_links:
-		link.handle_link_rotation(self, _pitch_angle)
+	#for link in _suspension_links:
+		#pass
+		#link.handle_link_rotation(self, _pitch_angle)
 
 
-	$LeftTreadInstance.speed = (
-		_linear_speed -
-		_angular_speed * 0.91 +
-		_pitch_speed * -1 * 0.17 * (PI-sin(_pitch_angle)))
-	$LeftTreadPath.q = abs(_pitch_angle) * 0.02 - _roll_angle * 0.012 + 0.3 - position.y * 1.5
-	$RightTreadInstance.speed = (
-		_linear_speed +
-		_angular_speed * 0.91 +
-		_pitch_speed * -1 * 0.17 * (PI-sin(_pitch_angle)))
-	$RightTreadPath.q = abs(_pitch_angle) * 0.02 + _roll_angle * 0.012 + 0.3 - position.y * 1.5
+	#$LeftTreadInstance.speed = (
+		#_linear_speed -
+		#_angular_speed * 0.91 +
+		#_pitch_speed * -1 * 0.17 * (PI-sin(_pitch_angle)))
+	#$LeftTreadPath.q = abs(_pitch_angle) * 0.02 - _roll_angle * 0.012 + 0.3 - position.y * 1.5
+	#$RightTreadInstance.speed = (
+		#_linear_speed +
+		#_angular_speed * 0.91 +
+		#_pitch_speed * -1 * 0.17 * (PI-sin(_pitch_angle)))
+	#$RightTreadPath.q = abs(_pitch_angle) * 0.02 + _roll_angle * 0.012 + 0.3 - position.y * 1.5
+	pass
+
+#func _handle_custom_physics(delta: float):
+	#var pitch_data = _calculate_spring_position(
+			#delta,
+			#_pitch_accel,
+			#_pitch_speed,
+			#_pitch_angle,
+			#_pitch_damped_spring,
+			#_current_accel.y * 2)
+	#_pitch_angle = pitch_data["position"]
+	#_pitch_speed = pitch_data["velocity"]
+	#_pitch_accel = pitch_data["acceleration"]
+#
+	##_roll_accel = (
+		##-_roll_angle * _roll_stiffness * delta -
+		##_roll_speed * _roll_damping * delta +
+		##_current_accel.x / 55)
+	##_roll_speed += _roll_accel
+	##_roll_angle += _roll_speed
+##
+	##_bounce_accel = (
+		##-_bounce_height * _bounce_stiffness * delta -
+		##_bounce_speed * _bounce_damping * delta)
+	##_bounce_speed += _bounce_accel
+	##_bounce_height += _bounce_speed
 
 
-func _handle_pitch(delta: float):
-	_pitch_accel = (
-		-_pitch_angle * _pitch_stiffness * delta -
-		_pitch_speed * _pitch_damping * delta +
-		_current_accel.y * 2)
-	_pitch_speed += _pitch_accel
-	_pitch_angle += _pitch_speed
-
-	_roll_accel = (
-		-_roll_angle * _roll_stiffness * delta -
-		_roll_speed * _roll_damping * delta +
-		_current_accel.x / 55)
-	_roll_speed += _roll_accel
-	_roll_angle += _roll_speed
-
-	_bounce_accel = (
-		-_bounce_height * _bounce_stiffness * delta -
-		_bounce_speed * _bounce_damping * delta)
-	_bounce_speed += _bounce_accel
-	_bounce_height += _bounce_speed
-
-
-func _tilt():
-	$Roll.rotation_degrees = Vector3(0, 0, _roll_angle)
-	$Roll/Pitch.rotation_degrees = Vector3(_pitch_angle, 0, 0)
+#func _tilt():
+	#$Roll.rotation_degrees = Vector3(0, 0, _roll_angle)
+	#$Roll/Pitch.rotation_degrees = Vector3(_pitch_angle, 0, 0)
 
 
 func _calc_accel():
@@ -125,3 +138,20 @@ func _handle_any_accel(current_speed: float, target_speed: float, accel: float, 
 			current_speed = target_speed
 	return current_speed
 
+
+#func _calculate_spring_position(
+		#delta: float,
+		#accel: float,
+		#vel: float,
+		#pos: float,
+		#damped_spring: DampedSpringParameters,
+		#external_accel: float = 0,
+		#):
+	#accel = (
+		#- pos * damped_spring.stiffness * delta
+		#- vel * damped_spring.damping * delta
+		#+ external_accel
+		#)
+	#vel += accel
+	#pos += vel
+	#return {"position": pos, "velocity": vel, "acceleration": accel}
