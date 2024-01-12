1f9e122afc071150bea62721ecd838165e840ce3
jonathan <jonathan.platner@outlook.com>
2024-01-11 20:57:40 -0800

    add fire rate and gun heat/overheat mechanism to cannon.gd

--- tank/local_signal_bus.gd
+++ tank/local_signal_bus.gd
@@ -10,9 +10,15 @@ signal linear_accel_changed(accel: float)
 signal angular_accel_changed(accel: float)
 signal barrel_end_of_travel(barrel_speed: float, turret_angle: float)
 
-signal cannon_fired(power: float, turret_angle: float, barrel_angle: float)
 
 signal drive_called(linear: float, angular: float)
 signal aim_called(pan: float, tilt: float)
 signal scan_called(pan: float)
+
+## Cannon signals.
 signal shoot_called(power: float)
+signal cannon_fired(power: float, turret_angle: float, barrel_angle: float)
+signal cannon_heat_changed(heat: float)
+signal can_shoot_changed(can_shoot: bool)
+signal reloaded_changed(reloaded: bool)
+signal overheated_changed(overheated: bool)
--- tank/sample_remote_control.gd
+++ tank/sample_remote_control.gd
@@ -17,7 +17,7 @@ func _process(_delta: float) -> void:
 	_local_signal_bus.scan_called.emit(
 		Input.get_axis('scan_right', 'scan_left'),
 	)
-	if Input.is_action_just_pressed('shoot'):
+	if Input.is_action_pressed('shoot'):
 		_local_signal_bus.shoot_called.emit(1)
 
 
